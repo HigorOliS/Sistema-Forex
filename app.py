9aa0a4961e29919cc83db099d9b2a4ea69eec1c4
HigorOliS <54632382+HigorOliS@users.noreply.github.com>
2025-08-20 17:00:20 -0300

    app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ elif st.session_state["tela"] == "resultado":
         "Par de moedas": ["EUR/USD", "USD/JPY", "GBP/USD", "USD/CHF", "AUD/USD"],
         "Horário sugerido": ["10:30:15", "11:45:30", "13:15:45", "15:20:10", "17:50:05"],
         "Ação": ["Vender", "Comprar", "Vender", "Vender", "Comprar"],
-        "Probabilidade(%)": [92, 96, 94, 90, 91]
+        "(%)": [92, 96, 94, 90, 91]
     }
     df = pd.DataFrame(dados)
 
