2fa45c76a94e7d2e3b7a9afc6b0ab92b5a0b1998
HigorOliS <54632382+HigorOliS@users.noreply.github.com>
2025-08-21 18:24:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ if st.button("✅ Gerar Análise"):
             '<span style="color:red">Vender</span>',
             '<span style="color:green">Comprar</span>'
         ],
-        "% Acerto": [92, 89, 94, 90, 91]
+        "% WIN": [92, 89, 94, 90, 91]
     }
     
     df = pd.DataFrame(data)
