c59e1e144fae5095b9fe9a12ff89cbe4054d210d
HigorOliS <54632382+HigorOliS@users.noreply.github.com>
2025-08-21 17:43:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.markdown(
     f"""
     <style>
     .stApp {{
-        background-image: url("https://github.com/HigorOliS/Sistema-Forex/blob/main/IMG_3894.jpeg");
+        background-image: url("https://raw.githubusercontent.com/HigorOliS/Sistema-Forex/main/IMG_3894.jpeg");
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
